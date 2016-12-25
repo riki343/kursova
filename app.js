50bb736d6ba17952ac6a40d41597e30a33704bd1
vlad <vladislav.kosko@gmail.com>
2016-12-25 20:58:40 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+angular
+    .module('kursova', [
+        'ui.router',
+        'angular-carousel'
+    ])
+    .config(AppConfig)
+    .run(AppRun)
+;
+
+function AppConfig($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        .state('home', {url: '/', templateUrl: 'app/home.html'})
+        .state('pro', {url: '/pro', templateUrl: 'app/pro.html'})
+        .state('air2', {url: '/air2', templateUrl: 'app/air2.html'})
+        .state('mini2', {url: '/mini2', templateUrl: 'app/mini2.html'})
+        .state('mini4', {url: '/mini4', templateUrl: 'app/mini4.html'})
+        .state('ios10', {url: '/iOS10', templateUrl: 'app/ios10.html'})
+        .state('accessories', {url: '/accessories', templateUrl: 'app/accessories.html'})
+    ;
+
+    $urlRouterProvider.otherwise('/');
+}
+
+AppConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+function AppRun() {
+    console.log('bootstrapped!');
+}
+
+AppRun.$inject = [];
